54f5b219e50abfa99b761f8894c334f73fda6e36
Marcin Szopa <marcin.pawel.szopa@gmail.com>
2024-06-12 18:39:07 +0200

    chore: fix maven set-up

--- src/main/java/StockExchange/Core/ITradingSystem.java
+++
@@ -1,5 +0,0 @@
-package StockExchange.Core;
-
-public interface ITradingSystem {
-
-}
--- src/main/java/StockExchange/Orders/ImmediateOrder.java
+++
@@ -1,8 +0,0 @@
-package StockExchange.Orders;
-
-public class ImmediateOrder extends GoodTillEndOfTurnOrder{
-    
-    public ImmediateOrder(int id,OrderType type, int investorId, int stockId, int amount, int limit, int firstRoundNo) {
-        super(id,type, investorId, stockId, amount, limit, firstRoundNo, firstRoundNo);
-    }
-}
