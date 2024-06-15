251fd069512ecc659130b67badb705bc3b8c07b7
Marcin Szopa <marcin.pawel.szopa@gmail.com>
2024-06-15 20:56:23 +0200

    BREAKING CHANGE: decrease value for UN orders

--- src/main/java/stockMarket/investors/InvestorHelper.java
+++ src/main/java/stockMarket/investors/InvestorHelper.java
@@ -37,7 +37,7 @@ public class InvestorHelper {
     private static int getRandomOrderMode(Random random) {
         // Define the probabilities for each value
         int[] values = {0, 1, 2, 3}; // Possible values
-        double[] probabilities = {0.1, 0.3, 0.3, 0.3};
+        double[] probabilities = {0.01, 0.33, 0.33, 0.33};
 
         // Calculate cumulative probabilities
         double[] cumulativeProbabilities = new double[probabilities.length];
